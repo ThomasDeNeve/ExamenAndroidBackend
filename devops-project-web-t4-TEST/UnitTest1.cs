8bf9ec75a74100b9e70cf5c98f7f48ec60d99c54
YvesVanduynslager <yves.vanduynslager.j1307@student.hogent.be>
2021-10-08 10:20:03 +0200

    add xunit test project

---
+++ devops-project-web-t4-TEST/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace devops_project_web_t4_TEST
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
