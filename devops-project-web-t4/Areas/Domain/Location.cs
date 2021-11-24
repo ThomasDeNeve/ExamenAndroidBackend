40c50cf55919fcc215feae5d130d797c4881d3c1
thomasboghaert <thomas.boghaert@student.hogent.be>
2021-11-24 13:41:52 +0100

    Added locationId property in meetingroom class

--- devops-project-web-t4/Areas/Domain/Location.cs
+++ devops-project-web-t4/Areas/Domain/Location.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -9,6 +10,7 @@ namespace devops_project_web_t4.Areas.Domain
 {
     public class Location
     {
+        private List<MeetingRoom> _rooms;
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
@@ -17,10 +19,30 @@ namespace devops_project_web_t4.Areas.Domain
         public int Number { get; set; }
         public string PostalCode { get; set; }
         public string Place { get; set; }
-        public List<MeetingRoom> MeetingRooms { get; set; } = new List<MeetingRoom>();
+        public List<MeetingRoom> MeetingRooms
+        {
+            get => _rooms;
+            set
+            {
+                AddLocationIdToMeetingRoom(value);
+            }
+        }
+
         public List<CoworkRoom> CoWorkRooms { get; set; } = new List<CoworkRoom>();
 
         public void AddMeetingRoom(MeetingRoom meetingRoom) => this.MeetingRooms.Add(meetingRoom);
+
+        private void AddLocationIdToMeetingRoom(List<MeetingRoom> value)
+        {
+            List<MeetingRoom> tempList = new List<MeetingRoom>();
+            foreach (MeetingRoom room in value)
+            {
+                MeetingRoom tempRoom = room;
+                tempRoom.LocationId = this.Id;
+                tempList.Add(tempRoom);
+            }
+            _rooms = tempList;
+        }
 
         //Onderstaande methode staat nu in CoworkRoom
         //public void AddCoWorkSeat(Seat coWorkSeat) => this.CoWorkSeats.Add(coWorkSeat);
