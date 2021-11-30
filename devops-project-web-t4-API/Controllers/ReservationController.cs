000a28f5c096c863d143523d7ed0b44564dd256a
YvesVanduynslager <yves.vanduynslager.j1307@student.hogent.be>
2021-11-30 14:58:37 +0100

    POST voor reservation

---
+++ devops-project-web-t4-API/Controllers/ReservationController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using devops_project_web_t4.Areas.Domain;
+using devops_project_web_t4.Data.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace devops_project_web_t4_API.Controllers
+{
+    [ApiConventionType(typeof(DefaultApiConventions))]
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservationController : ControllerBase
+    {
+        private readonly IReservationRepository _reservationRepository;
+        private readonly ICustomerRepository _customerRepository;
+        private readonly ISeatRepository _seatRepository;
+
+        public ReservationController(IReservationRepository reservationRepository, ICustomerRepository customerRepository, ISeatRepository seatRepository)
+        {
+            _reservationRepository = reservationRepository;
+            _customerRepository = customerRepository;
+            _seatRepository = seatRepository;
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Reservation> GetReservations()
+        {
+
+            return _reservationRepository.GetAll();
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Reservation> GetReservation(int id)
+        {
+            Reservation reservation = _reservationRepository.GetById(id);
+            return reservation == null ? NotFound() : reservation;
+        }
+
+        [HttpPost]
+        public ActionResult<Location> AddReservation(ReservationModel model)
+        {
+            Reservation reservation = new Reservation()
+            {
+                Customer = _customerRepository.GetById(model.CustomerId),
+                Seat = _seatRepository.GetById(model.SeatId)
+            };
+
+            _reservationRepository.Add(reservation);
+            _reservationRepository.SaveChanges();
+
+            return Ok(reservation);
+        }
+    }
+}
---
+++ devops-project-web-t4-API/ReservationModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace devops_project_web_t4_API
+{
+    public class ReservationModel
+    {
+        public int CustomerId { get; set; }
+        public int SeatId { get; set; }
+    }
+}
