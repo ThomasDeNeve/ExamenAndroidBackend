338520c757dfaafd42a21338ae4a549b001eddcf
thomasboghaert <thomas.boghaert@student.hogent.be>
2021-12-11 23:53:43 +0100

    Added click handler for navigation when selecting room

--- devops-project-web-t4/Pages/MeetingRoom/MeetingRoomHierOverzicht.razor.cs
+++ devops-project-web-t4/Pages/MeetingRoom/MeetingRoomHierOverzicht.razor.cs
@@ -10,11 +10,20 @@ namespace devops_project_web_t4.Pages.MeetingRoom
     {
         [Inject]
         private ILocationRepository LocationRepository { get; set; }
+        [Inject]
+        private NavigationManager NavigationManager { get; set; }
+
         private ICollection<devops_project_web_t4.Areas.Domain.MeetingRoom> _meetingRooms;
 
         protected override void OnInitialized()
         {
             _meetingRooms = LocationRepository.GetLocationByName("Hier").MeetingRooms;
+        }
+
+        public void ShowRoom(int roomId)
+        {
+            var selectedRoom = _meetingRooms.Single(room => room.Id == roomId);
+            NavigationManager.NavigateTo("meetingrooms/hier/" + selectedRoom.Name.ToLower());
         }
     }
 }
