226c6a370e38fb333f5c6e3e625d0c629855590e
YvesVanduynslager <yves.vanduynslager.j1307@student.hogent.be>
2021-12-13 11:49:18 +0100

    fix after merge

--- devops-project-web-t4/Areas/Controllers/IReservationController.cs
+++ devops-project-web-t4/Areas/Controllers/IReservationController.cs
@@ -11,7 +11,7 @@ namespace devops_project_web_t4.Areas.Controllers
         public void ConfirmCoworkReservation(int seatId, string username);
         public void ConfirmMeetingRoomReservation(int roomId, string userName);
 
-        public List<int> GetSeatIdsReservedForDate(DateTime date);
+        //public List<int> GetSeatIdsReservedForDate(DateTime date);
 
         //public List<Reservation> GetReservations();
     }
