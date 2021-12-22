52ee5751060d550c89a93de4f29ef82d73aba1f8
Thomas De Neve <thomas.deneve@student.hogent.be>
2021-12-22 22:08:00 +0100

    Updates to layout (reservations, facturation and general changes)

--- devops-project-web-t4/Pages/MeetingRoom/MeetingRoomLocationSelector.razor.cs
+++ devops-project-web-t4/Pages/MeetingRoom/MeetingRoomLocationSelector.razor.cs
@@ -13,8 +13,8 @@ namespace devops_project_web_t4.Pages.MeetingRoom
         private NavigationManager _navigationManager { get; set; }
 
         private string Message = @"Hou je van een groene omgeving? Of liever gezellig in het centrum van Aalst dicht bij het station? 
-            HIER.hebben we voor elk bedrijf wat wils.Onze twee locaties hebben samen zeven zalen met een capaciteit van maximum 120 personen.
-          Vergaderen met de board of director, teammeetings, leuke workshops of een presentatie geven, dat kan HIER.allemaal! 
+            HIER. hebben we voor elk bedrijf wat wils. Onze twee locaties hebben samen zeven zalen met een capaciteit van maximum 120 personen.
+          Vergaderen met de board of directors, teammeetings, leuke workshops of een presentatie geven, dat kan HIER. allemaal! 
             De meeting & opleidingszalen zijn voorzien van de allernieuwste snufjes, van Sonos boxen tot de nieuwste digi-schermen op de markt.
             Koffie, water en thee zijn steeds inbegrepen en audiovisueel materiaal is uiteraard ook aanwezig in de zaal.";
 
