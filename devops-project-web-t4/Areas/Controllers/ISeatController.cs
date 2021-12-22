ffbe52d5529547915128f6621cf642ea5e4e6d72
Lorenz Baele <lorenz.baele@student.hogent.be>
2021-12-22 18:16:52 +0100

    added documentation for controller

--- devops-project-web-t4/Areas/Controllers/ISeatController.cs
+++ devops-project-web-t4/Areas/Controllers/ISeatController.cs
@@ -7,6 +7,8 @@ namespace devops_project_web_t4.Areas.Controllers
 {
     public interface ISeatController
     {
+        /// <summary>Fetches all seats that have been reserved, also the locked ones.</summary>
+        /// <param name="date">The day. </param>
         public List<int> GetSeatIdsReservedForDate(DateTime date);
     }
 }
--- devops-project-web-t4/Areas/Controllers/ISubscriptionController.cs
+++ devops-project-web-t4/Areas/Controllers/ISubscriptionController.cs
@@ -8,8 +8,18 @@ namespace devops_project_web_t4.Areas.Controllers
 {
     public interface ISubscriptionController
     {
+        /// <summary>Finalizes a subsciption type for a user.</summary>
+        /// <param name="subName">The type of subscription</param>
+        /// <param name="userName">The name of the user</param>
         public void ConfirmSubscription(string subName, string userName);
+
+        /// <summary>Get the subscription for a certain customer.</summary>
+        /// <param name="customerName">The name of the customer</param>
+        /// <param name="month">The month where the subscription is active.</param>
         public List<CustomerSubscription> GetCustomerSubscriptions(string customerName = null, DateTime? month = null);
+
+        /// <summary>Check if the user has a valid subscription</summary>
+        /// <param name="userName">The name of the user</param>
         public bool HasActiveSub(string userName);
     }
 }
